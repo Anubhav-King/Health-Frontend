655ed27eeb80718b6c2ed9fae3bbff45a0de92be
Anubhav-King <awasthianubhav924@gmail.com>
2025-08-05 08:42:24 +0000

    URL Change

--- fitness-frontend/src/services/AuthService.js
+++ fitness-frontend/src/services/AuthService.js
@@ -1,9 +1,7 @@
 // src/services/AuthService.js
 import axios from 'axios';
 
-//const BASE_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000';
-const BASE_URL = "https://ebceebf3-81cc-4ce6-979e-387a3876db2e-00-1wfg3jhgn8y75.sisko.replit.dev";
-console.log("API Base URL:", process.env.BASE_URL);
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 
 export const login = async (mobile, password) => {
--- fitness-frontend/src/utils/api.js
+++ fitness-frontend/src/utils/api.js
@@ -1,3 +1,3 @@
 // src/utils/api.js
 
-export const BASE_URL = "https://ebceebf3-81cc-4ce6-979e-387a3876db2e-00-1wfg3jhgn8y75.sisko.replit.dev"; // Change this to your backend URL when deployed
+export const BASE_URL = process.env.REACT_APP_API_URL; // Change this to your backend URL when deployed
